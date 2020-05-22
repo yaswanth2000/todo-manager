cc40b79ba173703719f9aaa79290030540eca4f1
yaswanth <dvyaswanth123@gmail.com>
2020-05-22 13:45:15 +0530

    Use the resource routing convention for todos

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
 
 
 
-get "todos", to: "todos#index"
-get "todos/create", to:"todos#create"
-get "todos/:id", to: "todos#show"
+# get "todos", to: "todos#index"
+# post "todos", to:"todos#create"
+# get "todos/:id", to: "todos#show"
+resources :todos
 end
