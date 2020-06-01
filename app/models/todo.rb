389cedf72fc2ac6f4464e073e0144679ac42e8b8
yaswanth <dvyaswanth123@gmail.com>
2020-06-01 17:59:09 +0530

    Updated database migrations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,39 @@
+require "date"
+
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed =completed ? "[X]": "[]"
-    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
+
+
+
+
+  def to_formatted_string
+    status = completed ? "[x]" : "[]"
+    "#{id} #{due_date.to_s(:long)} #{todo_text} #{status}"
+  end
+
+  def self.of_user(user)
+    all.where(user_id: user.id)
+  end
+
+  def overdue?
+    due_date < Date.today and self.todo_status
+  end
+
+  def todo_status
+    self.completed
+  end
+
+  def self.overdue
+    all.where("due_date < ? and completed=?", Date.today, false)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date> ?", Date.today)
+  end
+  def self.completed
+    all.where("completed= ?", true)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 # get "todos", to: "todos#index"
 # post "todos", to:"todos#create"
 # get "todos/:id", to: "todos#show"
+get "/" => "home#index"
 resources :todos
 resources :users
 post "users/login",to: "users#login"
